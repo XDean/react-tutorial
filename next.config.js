68a1288654a5e3f8980e0e907658a268f97a0c73
dxu <dean.xu@asml.com>
2021-06-03 16:52:02 +0800

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withPlugins = require('next-compose-plugins');
+const withMDX = require('@next/mdx');
+
+module.exports = withPlugins([
+  [withMDX({
+      extension: /\.(md|mdx)$/,
+      options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+      },
+    }
+  )],
+])

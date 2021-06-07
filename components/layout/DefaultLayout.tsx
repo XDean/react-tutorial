9109838252a5369e9963b74742a7be46bf4e5ad6
dxu <dean.xu@asml.com>
2021-06-07 15:36:06 +0800

    add github link

--- components/layout/DefaultLayout.tsx
+++ components/layout/DefaultLayout.tsx
@@ -3,11 +3,17 @@ import {PropsWithChildren} from "react";
 export const DefaultLayout = (props: PropsWithChildren<{}>) => {
   return (
     <div className={'flex flex-col h-screen'}>
-      <div className={'w-full shadow-md p-2 border-b bg-white z-10 flex flex-row items-center'}>
-        <i className="fab fa-react text-4xl text-react animate-spin-slow duration-1000"/>
-        <div className={'inline text-4xl ml-2'}>
+      <div className={'w-full shadow-md p-2 border-b bg-white z-10 flex flex-row items-center text-4xl'}>
+        <i className="fab fa-react text-react animate-spin-slow duration-1000"/>
+        <div className={'inline ml-2'}>
           XDean's React Tutorial
         </div>
+        <div className={'flex-grow w-0'}/>
+        <a className={'cursor-pointer'}
+           target={'_blank'}
+           href={'https://github.com/XDean/react-tutorial'}>
+          <i className="fab fa-github"/>
+        </a>
       </div>
       <main className={'relative flex-grow w-full p-2 h-0'}>
         {props.children}

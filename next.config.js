f3f12d9a3f9ef3605d6ca449905189c15b911436
dxu <dean.xu@asml.com>
2021-06-07 14:18:55 +0800

    add code highlight

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withMDX = require('@next/mdx');
+const rehypePrism = require('@mapbox/rehype-prism');
 
 module.exports = withMDX({
   extension: /\.(md|mdx)$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: [],
+    rehypePlugins: [
+      rehypePrism
+    ],
   },
 })

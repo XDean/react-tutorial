74203a40ee3a60cc97a9a5e86175193e1894dbd8
dxu <dean.xu@asml.com>
2021-06-17 14:53:05 +0800

    fix comment

--- components/util/GithubComment.tsx
+++ components/util/GithubComment.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 // @ts-ignore
 import ReactUtterences from 'react-utterances'
+import {useRouter} from 'next/router'
 
 export const GithubComment = () => {
-  return <ReactUtterences type={'pathname'} repo={'XDean/blog-comment'}/>
+  const [show, setShow] = useState(true)
+  const router = useRouter();
+  useEffect(() => {
+    const listener = () => {
+      setShow(false);
+      setTimeout(() => setShow(true), 50)
+    }
+    router.events.on('routeChangeComplete', listener)
+    return () => {
+      router.events.off('routeChangeComplete', listener)
+    };
+  }, [router.events]);
+  return show && <ReactUtterences type={'pathname'} repo={'XDean/blog-comment'}/>
 }
